0b6939c48c3aa43980137fec09d6345b1f17e963
Justin <justin.thiele@hotmail.com>
2020-05-21 21:48:09 -0700

    Removed cart upon checkout

--- src/main/java/cova/rar/controller/OrderController.java
+++ src/main/java/cova/rar/controller/OrderController.java
@@ -5,9 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import cova.rar.entities.Cart;
@@ -16,6 +20,7 @@ import cova.rar.service.CookieMonster;
 import cova.rar.service.OrderService;
 
 @Controller
+@SessionAttributes("cart")
 public class OrderController {
 	
 	@Autowired
@@ -24,26 +29,26 @@ public class OrderController {
 	@Autowired
 	CookieMonster cookieMonster;
 	
+	@ModelAttribute("cart")
+	public Cart cart() {
+		return new Cart();
+	}
+	
 	
 	@RequestMapping(value = "/checkout", method = RequestMethod.GET)
-	public ModelAndView checkout(@SessionAttribute("cart") Cart cart, HttpServletRequest request) {
-
-
-		// add items from cart into orders database
-		
+	public ModelAndView checkout(@ModelAttribute("cart") Cart cart, WebRequest webReq,
+			SessionStatus status, HttpServletRequest request) {
 		
 		String userID = cookieMonster.getCookie("username", request).getValue();
 		
 		orderService.addCart(cart, userID);
 		
-		// update products inventory
-		// if product inventory is less than one -> do not update and mark product
-		// get orders from database
-
-		// send to orders view with orders from db and products that cant be displayed
+		status.setComplete();
+		webReq.removeAttribute("cart", WebRequest.SCOPE_SESSION);
+		
 		OrderHistory orderHistory = new OrderHistory();
 		orderHistory.setOrders(orderService.getOrderHistory(userID));
-		
+
 		return new ModelAndView("orderhistory", "orderhistory", orderHistory);
 	}
 	
